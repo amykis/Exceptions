38a604e0b8c830f3056aefdbf366703de6582f0f
Alexey Miakinin <amykis@mail.ru>
2024-10-21 23:50:16 +0300

    dz seminar_2 task_2

--- src/Seminar_2/DZ/Task_1.java
+++ src/Seminar_2/DZ/Task_1.java
@@ -1,2 +1,27 @@
-package Seminar_2.DZ;public class Task_1 {
+/*
+Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и
+возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению
+приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.
+*/
+
+package Seminar_2.DZ;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        float inputFloatNumber = inputFloat();
+        System.out.println(inputFloatNumber);
+    }
+
+
+    public static float inputFloat(){
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите десятичное число: ");
+        while (!console.hasNextFloat()){
+            System.out.println("Это не десятичное число!");
+            console.nextLine();
+        }
+        return console.nextFloat();
+    }
 }
--- src/Seminar_2/DZ/Task_2.java
+++ src/Seminar_2/DZ/Task_2.java
@@ -1,2 +1,22 @@
-package Seminar_2.DZ;public class Task_2 {
+/*
+Если необходимо, исправьте данный код (задание 2
+https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+*/
+
+package Seminar_2.DZ;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int[] intArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int d = 2;
+        if (intArray.length-1 < 8 ){
+            System.out.println("Массив содержит недостаточно элементов!");
+        } else if (d == 0) {
+            System.out.println("Деление на 0!");
+        }
+        else {
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        }
+    }
 }
