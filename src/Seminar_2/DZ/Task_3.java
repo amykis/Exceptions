b8b96c24459e75cb9e896bc4f6c9be92d060b3a0
Alexey Miakinin <amykis@mail.ru>
2024-10-22 00:00:28 +0300

    dz seminar_2 task_3

--- src/Seminar_2/DZ/Task_3.java
+++ src/Seminar_2/DZ/Task_3.java
@@ -8,23 +8,21 @@ package Seminar_2.DZ;
 public class Task_3 {
 
     public static void main(String[] args) throws Exception {
-        try {
             int a = 90;
             int b = 3;
-            System.out.println(a / b);
+            if (b != 0){
+                System.out.println(a / b);
+            }
             printSum(23, 234);
             int[] abc = { 1, 2 };
-            abc[3] = 9;
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!");
-        } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Массив выходит за пределы своего размера!");
+            if (3 < abc.length){
+                abc[3] = 9;
+            }
+            else {
+                System.out.println("Массив выходит за пределы своего размера!");
+            }
         }
-    }
-    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
-        System.out.println(a + b);
-    }
-
+        public static void printSum(Integer a,Integer b){
+            System.out.println(a + b);
+        }
 }
