45cead45a6ad71cc0999ddfd7357e7c8013ff131
Alexey Miakinin <amykis@mail.ru>
2024-10-22 11:29:23 +0300

    seminar_3 task_2

---
+++ src/Seminar_3/Lesson/Task_2/Counter.java
@@ -0,0 +1,22 @@
+package Seminar_3.Lesson.Task_2;
+
+import java.io.IOException;
+
+public class Counter implements AutoCloseable{
+    private int count;
+    private boolean closed = false;
+    public void add() throws IOException{
+        if (closed){
+            throw new IOException();
+        }
+        count++;
+    }
+    public String toString(){
+        return String.valueOf(count);
+    }
+
+    @Override
+    public void close() throws Exception {
+        closed = true;
+    }
+}
